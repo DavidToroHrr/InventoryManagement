85669d0e199b6d79fee2b48ff66511a3b47584c2
David Toro <davidesteban0736@.com>
2025-02-01 20:22:06 -0500

    se establece la estructura inicial

---
+++ VirtualStore/src/main/java/com/deth/virtualstore/VirtualStore.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.deth.virtualstore;
+
+import com.deth.gui.UserInterface;
+
+/**
+ *
+ * @author david
+ */
+public class VirtualStore {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        UserInterface userInterface=new UserInterface();
+        userInterface.setVisible(true);
+        userInterface.setLocationRelativeTo(null);
+        
+        
+    }
+}
